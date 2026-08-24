ea30ab1a63808f833344cbd23ab52e2026f13799
yarkfuneral-rgb <tarkfuneral@gmail.com>
2026-08-24 17:24:19 -0500

    Add custom AppIcon and live seeking for smooth iOS playback

--- CameraApp/HLSPlayerView.swift
+++ CameraApp/HLSPlayerView.swift
@@ -14,28 +14,47 @@ struct LiveAVPlayerView: UIViewControllerRepresentable {
 
     func makeUIViewController(context: Context) -> AVPlayerViewController {
         let controller = AVPlayerViewController()
-        let player = AVPlayer(url: url)
+        let asset = AVURLAsset(url: url)
+        let item = AVPlayerItem(asset: asset)
+        let player = AVPlayer(playerItem: item)
         player.automaticallyWaitsToMinimizeStalling = false
+        
         controller.player = player
-        controller.showsPlaybackControls = false // Oculta controles nativos para reproducción limpia sin botón play
+        controller.showsPlaybackControls = false
         controller.videoGravity = .resizeAspect
+        
+        context.coordinator.observe(item: item, player: player)
+        
         player.play()
         return controller
     }
 
     func updateUIViewController(_ uiViewController: AVPlayerViewController, context: Context) {
-        if let player = uiViewController.player,
-           let currentItem = player.currentItem,
-           let asset = currentItem.asset as? AVURLAsset,
-           asset.url != url {
-            let newPlayer = AVPlayer(url: url)
-            newPlayer.automaticallyWaitsToMinimizeStalling = false
-            uiViewController.player = newPlayer
-            newPlayer.play()
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+
+    class Coordinator: NSObject {
+        var statusObserver: NSKeyValueObservation?
+
+        func observe(item: AVPlayerItem, player: AVPlayer) {
+            statusObserver = item.observe(\.status, options: [.new]) { item, _ in
+                if item.status == .readyToPlay {
+                    player.seek(to: CMTime.positiveInfinity)
+                    player.play()
+                }
+            }
+        }
+
+        deinit {
+            statusObserver?.invalidate()
         }
     }
 
-    static func dismantleUIViewController(_ uiViewController: AVPlayerViewController, coordinator: ()) {
+    static func dismantleUIViewController(_ uiViewController: AVPlayerViewController, coordinator: Coordinator) {
+        coordinator.statusObserver?.invalidate()
         uiViewController.player?.pause()
         uiViewController.player = nil
     }
